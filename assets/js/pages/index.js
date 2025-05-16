e64831b081f455e59c23e77fd58d970a8d3f342d
moisesBarsoti <moises.barsot2@gmail.com>
2025-05-16 16:20:51 -0300

    A única coisa que faltam do menu é a responsividade e o botão de fechar e abrir o menu

--- assets/js/pages/index.js
+++ assets/js/pages/index.js
@@ -1,3 +1,6 @@
 alert("Antes de começarmos, precisamos de algumas informações.");
+// Pegando o nome do usuário
 nome = prompt("Qual é o seu nome?");
 user = document.querySelector("#user").innerHTML = `Olá, ${nome}`;
+
+// Abrir e fechar o menu
